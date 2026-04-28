173a1170c8956d71084c40a04e288ab2697b1c97
TangyKiwi <orange.panda.65@gmail.com>
2026-04-28 16:43:35 -0700

    Add main

--- pa0/pa0.py
+++ pa0/pa0.py
@@ -77,3 +77,6 @@ def PA0(path_to_user_reviews_csv):
     submit = users.describe().compute().round(2)    
     with open('results_PA0.json', 'w') as outfile: 
         json.dump(json.loads(submit.to_json()), outfile)
+
+if __name__ == "__main__":
+    PA0("user_reviews.csv")
